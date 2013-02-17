578dbb525426aa075b8a68925eca5928a9cfecc6
Tim Jones <tim@timjones.tw>
2013-02-17 22:43:38 +0800

    Fix validator

--- lib/mongoid/verbalize/validations/default_locale_presence.rb
+++ lib/mongoid/verbalize/validations/default_locale_presence.rb
@@ -3,9 +3,7 @@ module Mongoid
 		module Validations
 			class DefaultLocalePresenceValidator < ActiveModel::EachValidator
 				def validate_each(document, attribute, value)
-					field = document.fields[attribute.to_s]
-					translations = field.demongoize(value)
-          if translations.nil? || translations.value_for_locale([::I18n.default_locale]).blank?
+          if value.nil? || value.value_for_locale([::I18n.default_locale]).blank?
             document.errors.add(attribute, :blank, options)
           end
 				end
--- spec/mongoid/validations/default_locale_presence_spec.rb
+++ spec/mongoid/validations/default_locale_presence_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe Mongoid::Verbalize::Validations::DefaultLocalePresenceValidator do
 	describe '#validate_each' do
-		let(:product)   { Product.new }
-		let(:validator) { described_class.new(attributes: product.attributes) }
+		let(:product)    { Product.new }
+		let(:name_field) { product.fields['name'] }
+		let(:validator)  { described_class.new(attributes: product.attributes) }
 
 		context 'when the value is valid' do
-			before { validator.validate_each(product, :name, { 'en' => { 'value' => 'Foo' } }) }
+			before { validator.validate_each(product, :name, name_field.demongoize({ 'en' => { 'value' => 'Foo' } })) }
 
 			it 'adds no errors' do
 				puts product.errors[:name]
